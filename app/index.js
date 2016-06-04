2c8ebc32951c8d6915c16a2b2b1672ad76480fb7
Jaan Lambert <jaan.lambert@gmail.com>
2016-06-04 23:50:51 +0300

    Refactor

--- app/index.js
+++ app/index.js
@@ -5,8 +5,7 @@ import authData from '../dropbox-auth.json'
 const HOME_DIR = process.env.HOME || process.env.USERPROFILE
 const FILE_DIR = `${HOME_DIR}/Lockbox`
 
-authenticate(authData.key, authData.secret).then((code) => {
-  console.log(code)
+authenticate(authData.key, authData.secret).then(() => {
   return readDir(FILE_DIR) 
 }).then((files) => {
   console.log(files)
