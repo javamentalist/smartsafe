3e55cd3aeedc2e64dcdec85de4eac1adf682a41e
Peeter Kokk <peeter.kokk@gmail.com>
2016-10-18 13:53:53 +0300

    Replace the .then(success, fail) anti-pattern

--- client/app/index.js
+++ client/app/index.js
@@ -10,7 +10,7 @@ import crypto from 'crypto'
 import contractAddresses from '../contracts.json'
 
 const HOME_DIR = process.env.HOME || process.env.USERPROFILE
-const FILE_DIR = `${HOME_DIR}/Lockbox`
+const FILE_DIR = `${HOME_DIR}/SmartsafeClient`
 const IGNORED_FILES = ['.DS_Store', 'temp']
 
 const dropboxClient = new DropboxClient(authData.key, authData.secret)
@@ -18,19 +18,21 @@ const ethereumClient = new EthereumClient(contractAddresses)
 
 const syncFiles = (hashes, userFiles) => {
   const hashPromises = userFiles.map(filePath => {
-    return new Promise((resolve, reject) => {
-      const readStream = fs.createReadStream(`${FILE_DIR}/${filePath}`)
-      createHash(readStream).then((hash) => {
-        resolve({ filePath, hash })
+      fs.createReadStream(`${FILE_DIR}/${filePath}`)
+      .then(readStream => {
+        createHash(readStream)
+        .then(hash => filePath, hash)
       })
-    })
+      .catch(e => console.log(e))
   })
 
-  Promise.all(hashPromises).then(results => {
+  Promise.all(hashPromises)
+  .then(results => {
     results.forEach(fileData => {
       const { hash, filePath } = fileData
       if (hashes.indexOf(hash) === -1) {
-        dropboxClient.upload(`${FILE_DIR}/${filePath}`, `/${filePath}`).then(data => {
+        dropboxClient.upload(`${FILE_DIR}/${filePath}`, `/${filePath}`)
+        .then(data => {
           ethereumClient.addFile(hash, data.url, filePath)
         })
       }
@@ -38,7 +40,8 @@ const syncFiles = (hashes, userFiles) => {
 
     hashes.forEach(hash => {
       if (!_.find(results, { hash })) {
-        ethereumClient.getFile(hash).then(file => {
+        ethereumClient.getFile(hash)
+        .then(file => {
           const dlUrl = dropboxClient.getDirectDownloadLink(file.link)
           const filePath = dropboxClient.getFilePathFromUrl(dlUrl)
           const fileStream = fs.createWriteStream(`${FILE_DIR}/${filePath}`)
@@ -51,9 +54,10 @@ const syncFiles = (hashes, userFiles) => {
   })
 }
 
-dropboxClient.authenticate().then(() => {
-  return readDir(FILE_DIR)
-}).then((files) => {
+dropboxClient.authenticate()
+.then(() => {
+  return readDir(FILE_DIR)})
+  .then((files) => {
   const userFiles = files.filter((file) => {
     return IGNORED_FILES.indexOf(file) === -1
   })
