a0f02eb64bd8571c0f8887d2f6c9d9014b720dd6
Marina Nekrassova <marina.nekrassova@ttu.ee>
2016-10-25 23:27:38 +0300

    [#7] Setup ORM + JDBC drivers
    - some further project restructuring

--- server/smartsafe-rest-service/src/test/java/com/smartsafe/service/UserServiceSTest.java
+++ server/smartsafe-rest-service/src/test/java/com/smartsafe/service/UserServiceSTest.java
@@ -5,11 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.smartsafe.TestConfiguration;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration(classes = {TestConfiguration.class})
 public class UserServiceSTest {
 	
 	@Autowired
