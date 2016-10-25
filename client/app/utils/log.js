a426eea84477aa8b06ce526b57955a4d2a5a0da1
Taavi Tali <taavi.tali89@gmail.com>
2016-10-25 22:50:01 +0300

    [#53] Logging for client application
    add log.js

---
+++ client/app/utils/log.js
@@ -0,0 +1,21 @@
+// 1. use code below to use this logger:
+//  var logger = require('winston')
+//
+// 2. entrypoint js-file needs to have (index.js/server.js):
+// var logger = require('<path_to_this_file>')
+//
+// all other files need to use 1. to have winston configured
+
+var logger = require('winston');
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+  timestamp: true,
+  level: 'debug',
+  colorize: true
+});
+
+module.exports = logger
+
+logger.debug('tick-tock')
+logger.warn('something\'s fish')
+logger.error('achtung!')
