d45ebb4a5cfbfe7545a87091d54e306f309c79a3
Peeter Kokk <peeter.kokk@gmail.com>
2016-10-26 15:18:05 +0300

    Refactor index.js #50

--- client/app/index.js
+++ client/app/index.js
@@ -17,34 +17,64 @@ const dropboxClient = new DropboxClient(authData.key, authData.secret);
 const ethereumClient = new EthereumClient(contractAddresses);
 
 const syncFiles = (hashes, userFilesLocations) => {
-    userFiles = getUserFiles(userFilesLocations);
-    hashesOfFiles = generateHashesForUserFiles(userFiles);
+    const userFiles = getUserFiles(userFilesLocations);
+    const userFilesDataForUpload = generateHashesForUserFiles(userFiles);
+    const dropboxLinks = uploadUserFileData(userFilesDataForUpload, hashes);
     la(hashesOfFiles);
 };
 
 const getUserFiles = function(userFilesLocations) {
-    return userFilesLocations.map(filePath => {
-        const readStream = fs.createReadStream(`${FILE_DIR}/${filePath}`);
-        return new Promise((resolve, reject) => {
-            resolve({filePath, readStream})
-        })
-    })
+    try {
+        return userFilesLocations.map(filePath => {
+            const readStream = fs.createReadStream(`${FILE_DIR}/${filePath}`);
+            return new Promise((resolve, reject) => {
+                resolve({filePath, readStream})
+            })
+        }).catch(console.log.bind(console))
+    } catch (err) {
+        console.log(err).bind(console);
+        return Promise.reject(err)
+    }
 };
 
 const generateHashesForUserFiles = function(userFiles) {
-    Promise.all(userFiles).then(results => {
-        Promise.all(results.map((filePath, readStream) => {
-            return new Promise((resolve, reject) => {
-                const fileHash = createHash(readStream);
-                resolve ({filePath,fileHash})
+    try {
+        userFiles.map(userFileData => {
+            Promise.resolve(userFileData).then((filePath, readStream) => {
+                return new Promise((resolve, reject) => {
+                    const fileHash = createHash(readStream);
+                    resolve({filePath, fileHash});
+                })
             })
-        }))
-    }).then((filePath, hash) => {
-        return {filePath, hash}
-    })
+        }).catch(console.log.bind(console))
+    } catch (err) {
+        console.log(err).bind(console);
+        return Promise.reject(err)
+    }
+};
+
+const uploadUserFileData = function(userFilesDataForUpload, hashes) {
+    try {
+        userFilesDataForUpload.map(userFileData => {
+            Promise.resolve(userFileData).then((filePath, fileHash) => {
+                if (hashes.indexOf(fileHash) === -1) {
+                    throw new Error("Filehash " + fileHash + " cannot be found")
+                }
+                return new Promise((resolve, reject) => {
+                    const dropboxLinks = dropboxClient.upload(`${FILE_DIR}/${filePath}`, `/${filePath}`);
+                    resolve(dropboxLinks);
+                })
+            })
+        }).catch(console.log.bind(console))
+    } catch (err) {
+        console.log(err).bind(console);
+        return Promise.reject(err)
+    }
 };
 
 const la = function (hashesOfFiles) {
+    ethereumClient.addFile(hash, data.url, filePath)
+    resolve({filePath, fileHash})
     Promise.all(hashPromises).then(results => {
         results.forEach(fileData => {
             const {hash, filePath} = fileData;
