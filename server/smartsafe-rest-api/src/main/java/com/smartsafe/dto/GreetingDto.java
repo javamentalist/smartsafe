133044620f43114fcdc91c46e9c6705a1147ae19
Marina Nekrassova <marina.nekrassova@ttu.ee>
2016-10-29 22:05:14 +0300

    [#13] Registration of a new user
    - further refactoring

--- server/smartsafe-rest-api/src/main/java/com/smartsafe/dto/GreetingDto.java
+++ server/smartsafe-rest-api/src/main/java/com/smartsafe/dto/GreetingDto.java
@@ -1,20 +1,12 @@
 package com.smartsafe.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class GreetingDto {
 
     private final long id;
     private final String content;
-
-    public GreetingDto(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
 }
