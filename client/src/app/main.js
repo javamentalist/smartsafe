c5732de36227113af79b6443f233fc74bf921136
Liisi Mõtshärg <liiziest@gmail.com>
2016-10-30 19:52:54 +0200

    Move route definitions to separate file

--- client/src/app/main.js
+++ client/src/app/main.js
@@ -22,7 +22,6 @@ function createWindow() {
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-	console.log(`${__dirname}`)
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
--- client/src/app/screens/app/index.js
+++ client/src/app/screens/app/index.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import {Router, Route, IndexRoute, Link, hashHistory} from 'react-router'
-
 import Sidebar from './components/sidebar'
 
-import Files from './screens/files'
-import Settings from './screens/settings'
-// TODO use separate file for routes import routes from '../../config/routes'
-// Other imports Component class
 class App extends React.Component {
 
-  // render method is most important render method returns JSX template
   render() {
     return (
       <div className="pane-group">
@@ -27,15 +20,4 @@ class App extends React.Component {
   }
 }
 
-const routes = (
-  <Route path="/" component={App}>
-    <IndexRoute component={Files}/>
-    <Route path="files" component={Files}>
-      <Route path="mine" component={Files}/>
-      <Route path="shared-with-me" component={Files}/>
-    </Route>
-    <Route path="settings" component={Settings}/>
-  </Route>
-)
-
-render((<Router history={hashHistory} routes={routes}/>), document.getElementById('content'))
+export default App
