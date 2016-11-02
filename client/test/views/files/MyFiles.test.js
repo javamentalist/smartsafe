085addd52cc09a5b0a53707d1847080f7d323fdd
Liisi Mõtshärg <Liisi Mtshrg>
2016-11-03 00:36:57 +0200

    Replace one long broken test with 2 better ones

--- client/test/views/files/MyFiles.test.js
+++ client/test/views/files/MyFiles.test.js
@@ -22,20 +22,13 @@ describe('MyFiles', () => {
     wrapper.should.have.descendants('div')
   })
 
-  it('should have one first level title (h1) with text "Files"', () => {
-    wrapper.should.
-
-      h1Array
-      .should
-      .have
-      .lengthOf(1)
-
-    const h1 = h1Array[0];
-
-    (h1.innerHTML)
-      .should
-      .equal('Files')
+  it('should have one first level title (h1)', () => {
+    wrapper.should.have.exactly(1).descendants('h1')
   })
+
+  it('should have title with text "Files"', () => {
+    (wrapper.find('h1').first()).should.have.text('Files')
+  });
 
   it('should have a FileTable', () => {
     wrapper.should.have.descendants(FileTable)
