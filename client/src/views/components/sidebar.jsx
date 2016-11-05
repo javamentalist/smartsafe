0ee17386dd35b88ee9c6cc9d6159772bd7d73d5b
Liisi Mõtshärg <Liisi Mtshrg>
2016-11-05 23:35:09 +0200

    Button, Sidebar, FileTable to functional stateless components

--- client/src/views/components/sidebar.jsx
+++ client/src/views/components/sidebar.jsx
@@ -1,32 +1,29 @@
 import React from 'react'
-import {render} from 'react-dom'
-import {Link} from 'react-router'
+import { render } from 'react-dom'
+import { Link } from 'react-router'
 
-export default class Sidebar extends React.Component {
 
-  render() {
-    return (
-      <nav className="nav-group">
-        <Link to="/" className="nav-group-item">
-          <span className="icon icon-folder"></span>
-          Files
+const Sidebar = () => (
+  <nav className="nav-group">
+    <Link to="/" className="nav-group-item">
+      <span className="icon icon-folder"></span>
+      Files
         </Link>
-        <div className="nav-group-level-2">
-          <Link to="/files/mine" className="nav-group-item">
-            <span className="icon icon-folder"></span>
-            My files
+    <div className="nav-group-level-2">
+      <Link to="/files/mine" className="nav-group-item">
+        <span className="icon icon-folder"></span>
+        My files
           </Link>
-          <Link to="/files/shared-with-me" className="nav-group-item">
-            <span className="icon icon-folder"></span>
-            Shared with me
+      <Link to="/files/shared-with-me" className="nav-group-item">
+        <span className="icon icon-folder"></span>
+        Shared with me
           </Link>
-        </div>
-        <Link to="/settings" className="nav-group-item">
-          <span className="icon icon-cog"></span>
-          Settings
+    </div>
+    <Link to="/settings" className="nav-group-item">
+      <span className="icon icon-cog"></span>
+      Settings
         </Link>
-      </nav>
-    );
-  }
+  </nav>
+)
 
-}
+export default Sidebar
--- client/src/views/files/components/file-table-row.jsx
+++ client/src/views/files/components/file-table-row.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export default class FileTableRow extends React.Component {
 
-  render() {
-    const file = this.props.file
-    return (
-      <tr>
-        <td>
-          <span className="icon icon-share"></span>&nbsp;
+const FileTableRow = ({file}) => (
+  <tr>
+    <td>
+      <span className="icon icon-share"></span>&nbsp;
           {file.shared || 0}
-        </td>
-        <td>{file.name}</td>
-      </tr>
-    );
-  }
-}
+    </td>
+    <td>{file.name}</td>
+  </tr>
+)
+
+export default FileTableRow
