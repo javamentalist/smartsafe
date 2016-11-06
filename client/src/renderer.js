92f6ad8f8d245d806bf0b41c1d527f89c2abd04e
Liisi Mõtshärg <liiziest@gmail.com>
2016-11-06 18:53:42 +0200

    Remove babelify, make electron use ES6 files directly

---
+++ client/src/renderer.js
@@ -0,0 +1,10 @@
+// index.js (renderer process)
+
+// Initialize Electron renderer function.
+// This magic is needed, because electron doesn't understand ES6 yet
+// and bundling with browserify caused errors when accessing file system
+// ('fs.readFileSync is not a function')
+// See here: https://github.com/electron/electron/issues/1611#issuecomment-104808431
+
+require('babel-register')
+require('./app-index.js')
