fc6debbea5cb174e3ba8bfa68e306aafbeb47d60
Liisi Mõtshärg <liiziest@gmail.com>
2016-11-14 00:09:49 +0200

    Rename FleList to UserFileList

--- client/src/views/files/index.js
+++ client/src/views/files/index.js
@@ -1,8 +1,8 @@
 // Export defaults as named exports
 export {default as FileTable} from './components/FileTable.jsx'
-export {default as FileList} from './FileList.jsx'
+export {default as UserFileList} from './UserFileList.jsx'
 export {default as FileDetail} from './FileDetail.jsx'
 
 // Also export named exports with a different name (useful for testing)
-export {FileList as FileListUndecorated} from './FileList.jsx'
+export {UserFileList as UserFileListUndecorated} from './UserFileList.jsx'
 export {FileDetail as FileDetailUndecorated} from './FileDetail.jsx'
