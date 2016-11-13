64e83327e2f6e7178ea5785eb65d82917d4d8730
Peeter Kokk <peeter.kokk@gmail.com>
2016-11-13 20:05:05 +0200

    Add functionality to index.js #59

--- client/src/index.js
+++ client/src/index.js
@@ -11,65 +11,166 @@ import contractAddresses from '../contracts.json'
 
 const HOME_DIR = process.env.HOME || process.env.USERPROFILE;
 const FILE_DIR = `${HOME_DIR}/SmartsafeClient`;
+const TEMP_DIR = `${HOME_DIR}/SmartsafeClient`;
+
 const IGNORED_FILES = ['.DS_Store', 'temp'];
 
 const dropboxClient = new DropboxClient(authData.key, authData.secret);
 const ethereumClient = new EthereumClient(contractAddresses);
 
-const syncFiles = (filesHashes, userFilesLocations) => {
-    const userFiles = getUserFiles(userFilesLocations);
-    const userFilesDataForUploadToDropbox = generateDropboxUploadDataForUserFiles(userFiles);
-    uploadUserFileData(userFilesDataForUploadToDropbox, filesHashes);
-    downloadMissingSharedFiles(userFilesDataForUploadToDropbox, filesHashes);
-};
+function logDebug(err) {
+    winston.log('debug', err)
+}
 
-const getUserFiles = function(userFilesLocations) {
-    try {
-        return userFilesLocations.map(filePath => {
-            const readStream = fs.createReadStream(`${FILE_DIR}/${filePath}`);
-            return new Promise((resolve, reject) => {
-                resolve({filePath:filePath, fileInfo:readStream})
-            })
-        }).catch(err => {
-            console.log(err).bind(console);
-            Promise.reject(err)
+function logError(err) {
+    winston.log('error', err)
+}
+
+function synchronizeUserFiles(filesHashesFromEth, localFilesFullPaths) {
+    /// Upload local files
+    // Promise.resolve(localFilesFullPaths).then(userFilesFullPaths => {
+    //     return Promise.all(userFilesFullPaths.map(userFileFullPath => {
+    //         return prepareFileDataForFiles(userFileFullPath);
+    //     }));
+    // }).then(localFilesDataToDropbox => {
+    //     return Promise.all(localFilesDataToDropbox.map(localFileData => {
+    //         const localFileName = localFileData.fileName;
+    //         const localFileHash = localFileData.fileInfo;
+    //         if (!fileMetaDataUploadedToEth(localFileHash, filesHashesFromEth)) {
+    //             return uploadLocalFilesToDropbox(localFileName, localFileHash)
+    //         }
+    //         return Promise.resolve()
+    //
+    //     }))
+    // }).then(filesDataToEth => {
+    //     if (filesDataToEth.length === -1) {
+    //         return Promise.resolve()
+    //     }
+    //
+    //     return Promise.all(filesDataToEth.map(fileDataToEth => {
+    //         if (fileDataToEth == null) return Promise.resolve();
+    //         return uploadLocalFileMetaDataToEth(fileDataToEth)
+    //     }));
+    // }).catch(err => {
+    //     logError(err);
+    // });
+
+    /// Download missing local files
+    Promise.resolve(localFilesFullPaths).then(userFilesFullPaths => {
+        return Promise.all(userFilesFullPaths.map(userFileFullPath => {
+            return prepareFileDataForFiles(userFileFullPath);
+        }));
+    }).then(localFileDatas => {
+        return Promise.resolve(filesHashesFromEth).then(filesEthHashes => {
+            Promise.all(filesEthHashes.map(fileEthHash => {
+                getFilesOnEthNotLocallyPresent(localFileDatas, fileEthHash);
+            }))
         })
-    } catch (err) {
-        console.log(err).bind(console);
-        return []
-    }
-};
+    }).then(fileEthMetaDatas => {
+        logError(fileEthMetaDatas);
+        if (fileEthMetaDatas.length === -1) {
+            return Promise.resolve()
+        }
 
-const generateDropboxUploadDataForUserFiles = function(userFiles) {
-    try {
-        return userFiles.map(userFile => {
-            Promise.resolve(userFile).then((filePath, fileInfo) => {
-                return new Promise((resolve, reject) => {
-                    const fileHash = createHash(fileInfo);
-                    resolve({filePath:filePath, fileInfo:fileHash});
-                })
+        return Promise.all(fileEthMetaDatas.map(fileEthMetaData => {
+            if (fileEthMetaData == null) return Promise.resolve();
+            return Promise.resolve(downloadFileFromDropbox(fileEthMetaData));
+        }))
+    }).catch(err => {
+        logError(err);
+    })
+
+}
+function getFilesOnEthNotLocallyPresent(localFileDatas, fileEthHash) {
+    return new Promise((resolve, reject) => {
+            for (let localFileDataIndex = 0; localFileDataIndex < localFileDatas.length; ++localFileDataIndex) {
+                const localFileData = localFileDatas[localFileDataIndex];
+
+                const localFileHash = localFileData.fileInfo;
+                if (localFileHash === fileEthHash) {
+                    return resolve()
+                }
+            }
+            return resolve(downloadMetaDataFromEthWithHash(fileEthHash));
+    });
+}
+
+function downloadMetaDataFromEthWithHash(fileHash) {
+    return new Promise((resolve, reject) => {
+        (ethereumClient.findFileDropboxDataFromEthChain(fileHash))
+            .then(fileMetaDataFromEth => {
+                return resolve(fileMetaDataFromEth)
             })
-        }).catch(err => {
-            console.log(err).bind(console);
-            Promise.reject(err)
-        })
-    } catch (err) {
-        console.log(err).bind(console);
-        return []
-    }
-};
+    })
+}
 
-const uploadUserFileData = function(userFilesDataForUploadToDropbox, filesHashes) {
-    const userFilesDataForUploadToEth
-        = uploadUserFileToDropbox(userFilesDataForUploadToDropbox, filesHashes);
-    const ethUploading = uploadUserFileMetaDataToEth(userFilesDataForUploadToEth, filesHashes);
-    ethUploading.map(ethUpload => {
-        Promise.resolve(ethUpload).catch(err => {
-            console.log(err).bind(console);
-            Promise.reject(err)
+function fileMetaDataUploadedToEth(hash, ethHashes) {
+    return ethHashes.indexOf(hash) !== -1;
+}
+
+function prepareFileDataForFiles(filePath) {
+    return new Promise((resolve, reject) => {
+        const fileName = getFileNameFromFilePath(filePath);
+        const readStream = fs.createReadStream(getFullPathForFileName(fileName));
+
+        readStream.on('error', (error) => {
+            if (error.code == 'ENOENT') {
+                return ""
+            }
+            throw error
+        });
+
+        return createHashForFile(readStream).then(fileData => {
+            return resolve({fileName: fileName, fileInfo: fileData})
+        })
+    })
+}
+
+function getFullPathForFileName(fileName) {
+    return `${FILE_DIR}/${fileName}`;
+}
+
+function getFileNameFromFilePath(filePath) {
+    return filePath.substring(filePath.lastIndexOf(FILE_DIR) + FILE_DIR.length + 1, filePath.length);
+}
+
+function uploadLocalFilesToDropbox(fileName, fileHash) {
+    return new Promise((resolve, reject) => {
+        Promise.resolve(dropboxClient.upload(`${FILE_DIR}/${fileName}`, `/${fileName}`))
+            .then(responseJson => {
+                return resolve({
+                    fileName: fileName,
+                    fileHash: fileHash,
+                    fileSharedLink: responseJson.url
+                });
+            })
+
+
+    })
+}
+
+function uploadLocalFileMetaDataToEth(fileData) {
+    return new Promise((resolve, reject) => {
+        const fileName = fileData.fileName;
+        const fileHash = fileData.fileHash;
+        const fileDropboxSharedLink = fileData.fileSharedLink;
+        ethereumClient.addFileMetaData(fileHash, fileDropboxSharedLink, fileName).then(()=> {
+            return resolve()
+        });
+
+    })
+}
+
+function downloadFileFromDropbox(fileMetaDataFromEth) {
+    return new Promise((resolve, reject) => {
+        const downloadUrl = DropboxClient.getDirectDownloadLink(fileMetaDataFromEth.link);
+        const fileName = DropboxClient.getFileNameFromUrl(downloadUrl);
+        const fileStream = fs.createWriteStream(`${FILE_DIR}/${fileName}`);
+        https.get(downloadUrl, fileToDownload => {
+            fileToDownload.pipe(fileStream)
         });
     });
-};
+}
 
 const fileHasBeenHashed = function(fileHash, filesHashes) {
     return filesHashes.indexOf(fileHash) !== -1;
