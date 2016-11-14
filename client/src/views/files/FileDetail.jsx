3c8660afaa9f4dda13e0f05c7c3c2506c4775599
Liisi Mõtshärg <liiziest@gmail.com>
2016-11-14 02:51:38 +0200

    [#56] First version of file detail view (no sharing info yet)

--- client/src/views/files/FileDetail.jsx
+++ client/src/views/files/FileDetail.jsx
@@ -7,6 +7,7 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 import * as Actions from '../../actions'
+import {formatDate} from '../../utils/displayUtils'
 
 // named export. Useful for testing only component itself without store logic
 export class FileDetail extends React.Component {
@@ -15,8 +16,73 @@ export class FileDetail extends React.Component {
     super(params);
   }
 
+  renderPageTitle(file) {
+    return (
+      <div className="row">
+        <div className="col-xs-12">
+          {file && <h2>{file.name}</h2>}
+        </div>
+      </div>
+    );
+  }
+
+  renderFileInfo(file) {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-3">
+            <strong>Id</strong>
+          </div>
+          <div className="col-xs-9">{file.id}</div>
+        </div>
+        <div className="row">
+          <div className="col-xs-3">
+            <strong>Name</strong>
+          </div>
+          <div className="col-xs-9">{file.name}</div>
+        </div>
+        <div className="row">
+          <div className="col-xs-3">
+            <strong>Path</strong>
+          </div>
+          <div className="col-xs-9">{file.path_display}</div>
+        </div>
+        <div className="row">
+          <div className="col-xs-3">
+            <strong>Client modified</strong>
+          </div>
+          <div className="col-xs-9">{formatDate(file.client_modified)}</div>
+        </div>
+        <div className="row">
+          <div className="col-xs-3">
+            <strong>Server modified</strong>
+          </div>
+          <div className="col-xs-9">{formatDate(file.server_modified)}</div>
+        </div>
+      </div>
+    );
+  }
+
+  renderSharedInfo(file) {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-12">
+            <h3>Shared with</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12">
+            <p style={{"color": "red"}}>Here be sharing info</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
-    let file = this.props.file
+    let file = this.props.file;
+    console.log(file)
     return (
       <div className="row">
         <div className="col-xs-12">
@@ -29,17 +95,15 @@ export class FileDetail extends React.Component {
                 this
                   .context
                   .router
-                  .goBack();
+                  .push('/files');
               }}/>
             </div>
           </div>
-          <div className="row">
-            <div className="col-xs-12">
-              {file
-                ? <h2>{file.name}</h2>
-                : <h2>No file</h2>}
-            </div>
-          </div>
+          {file
+            ? this.renderPageTitle(file)
+            : <h2>No file</h2>}
+          {file && this.renderFileInfo(file)}
+          {file && this.renderSharedInfo(file)}
         </div>
       </div>
     )
