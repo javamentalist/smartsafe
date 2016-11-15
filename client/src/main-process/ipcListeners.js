5f9e8dc3344bca08d800f1e7fb3211ec0eef567e
Liisi Mõtshärg <liiziest@gmail.com>
2016-11-16 00:08:16 +0200

    [#36] Require eth/db code from electron main.js

---
+++ client/src/main-process/ipcListeners.js
@@ -0,0 +1,71 @@
+import { ipcMain, dialog } from 'electron'
+import _ from 'lodash'
+import winston from '../utils/log'
+
+import { dropboxClient } from '../main'
+
+// Message listeners
+// TODO make channel names constants (channel name is first argument of .on())
+// Read: http://electron.atom.io/docs/api/ipc-main/
+
+ipcMain.on('open-file-dialog-async', (event) => {
+  dialog.showOpenDialog({
+    properties: ['openFile']
+  }, function(filePaths) {
+    if (filePaths && filePaths.length > 0) {
+      let filePath = filePaths[0];
+      logDebug(`File chosen: ${filePath}`);
+      // Alert renderer that file was chosen
+      event.sender.send('file-chosen-async', filePath);
+    } else {
+      logDebug('No file chosen');
+    }
+  })
+  logDebug('Dialog open called');
+})
+
+ipcMain.on('upload-file-async', (event, file) => {
+  winston.log('info', `Uploading file: ${file.name} from ${file.path}`)
+// When done, send response
+})
+
+ipcMain.on('get-files-from-dropbox-async', (event) => {
+
+  dropboxClient.listFolder().then((result) => {
+    let files = Array.from(result);
+
+    if (files.length !== 0) {
+      logDebug(`Found ${files.length} file(s)`);
+      files.forEach(res => {
+        logDebug(`- Name: ${res.name}`);
+      });
+    } else {
+      logDebug('Found no files in app folder');
+    }
+    event.sender.send('set-dropbox-files-async', files);
+    return files;
+  }).catch((reject) => {
+    logDebug(reject.error);
+  });
+});
+
+
+// Logging
+
+ipcMain.on('log-async', (event, ...args) => {
+  if (args.length === 1) {
+    logDebug(args[0]);
+  } else if (args.length > 1) {
+    log(args[0], _.drop(args, 1));
+  }
+});
+
+function logDebug(err) {
+  winston.log('debug', err)
+}
+function logError(err) {
+  winston.log('error', err)
+}
+function log(level, ...args) {
+  winston.log(level, _.join(args, ' '));
+}
--- client/src/main.js
+++ client/src/main.js
@@ -5,16 +5,17 @@
 // BrowserWindow - module to create native browser window.
 // ipcMain - listens to renderer process and sends messages to it
 // dialog - native fail dialog
-import {app, BrowserWindow, ipcMain, dialog} from 'electron'
+import { app, BrowserWindow } from 'electron'
 
 import winston from './utils/log'
-import _ from 'lodash'
 
-import authData  from '../dropbox-auth.json'
-import DropboxClient from './api/dropboxApi'
+import authData from '../dropbox-auth.json'
+import contractAddresses from '../contracts.json'
+import DropboxClient from './api/dropboxApi.js'
+import EthereumClient from './api/ethereum/ethereumApi.js'
 
-
-const dropboxClient = new DropboxClient(authData.key, authData.secret)
+export const dropboxClient = new DropboxClient(authData.key, authData.secret)
+export const ethereumClient = new EthereumClient(contractAddresses)
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -33,7 +34,9 @@ function createWindow() {
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
 
-  startDropbox();
+  dropboxClient.authenticate()
+    .then(() => winston.log('info', 'Dropbox authenticated'))
+    .catch((err) => winston.log('error', err));
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
@@ -67,77 +70,8 @@ app.on('activate', function() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-
-function startDropbox() {
-  dropboxClient.authenticate()
-    .then(() => log('info', 'Dropbox authenticated'))
-    .catch((err) => log('error', err));
-}
-
-// Message listeners
-// TODO make channel names constants (channel name is first argument of .on())
-// Read: http://electron.atom.io/docs/api/ipc-main/
-
-ipcMain.on('open-file-dialog-async', (event) => {
-  dialog.showOpenDialog({
-    properties: ['openFile']
-  }, function(filePaths) {
-    if (filePaths && filePaths.length > 0) {
-      let filePath = filePaths[0];
-      logDebug(`File chosen: ${filePath}`);
-      // Alert renderer that file was chosen
-      event.sender.send('file-chosen-async', filePath);
-    } else {
-      logDebug('No file chosen');
-    }
-  })
-  logDebug('Dialog open called');
-})
-
-ipcMain.on('upload-file-async', (event, file) => {
-  winston.log('info', `Uploading file: ${file.name} from ${file.path}`)
-
-// When done, send response
-// event.sender.send('asynchronous-reply', 'contents')
-})
-
-//this.props.actions.setAuthStatus(true);
-
-ipcMain.on('get-files-from-dropbox-async', (event) => {
-
-  dropboxClient.listFolder().then((result) => {
-    let files = Array.from(result);
-
-    if (files.length !== 0) {
-      logDebug(`Found ${files.length} file(s)`);
-      files.forEach(res => {
-        logDebug(`- Name: ${res.name}`);
-      });
-    } else {
-      logDebug('Found no files in app folder');
-    }
-    event.sender.send('set-dropbox-files-async', files);
-    return files;
-  }).catch((reject) => {
-    logDebug(reject.error);
-  });
-});
-
-ipcMain.on('log-async', (event, ...args) => {
-  if (args.length === 1) {
-    logDebug(args[0]);
-  } else if (args.length > 1) {
-    log(args[0], _.drop(args, 1));
-  }
-});
-
-function logDebug(logString) {
-  winston.log('debug', logString)
-}
-function log(level, args) {
-  winston.log(level, _.join(args, ' '));
-}
+require('./main-process/ipcListeners')
+require('./main-process/fileSynchronization')
 
 
 require('electron-reload')(__dirname);
