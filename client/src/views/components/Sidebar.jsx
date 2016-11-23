0b0448a458043da4c19a20fe309962c85076ba1c
Liisi Mõtshärg <liiziest@gmail.com>
2016-11-24 01:52:06 +0200

    [#67] Fixed sidebar look

--- client/src/views/components/Sidebar.jsx
+++ client/src/views/components/Sidebar.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import {Link} from 'react-router'
+import React from 'react';
+import { Link } from 'react-router';
+import FontIcon from 'material-ui/FontIcon';
 
 const Sidebar = () => (
-  <nav className="nav-group">
-    <Link to="/" className="nav-group-item">
-      <span className="icon icon-folder"></span>
-      Files
-    </Link>
-    <Link to="/settings" className="nav-group-item">
-      <span className="icon icon-cog"></span>
-      Settings
-    </Link>
-  </nav>
-)
+    <div>
+      <div className="logo__border">
+        <div className="logo__icon"></div>
+      </div>
+      <nav>
+        <Link to="/">
+        <FontIcon className="fa fa-folder-o nav__nav-icon" /> Files
+        </Link>
+        <Link to="/settings">
+        <FontIcon className="fa fa-cogs nav__nav-icon" /> Settings
+        </Link>
+      </nav>
+    </div>
+);
 
-export default Sidebar
+export default Sidebar;
